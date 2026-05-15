f57f5e2e58586e61df8e24ae8eca8e2be2a697b2
Loyal <loyal3371@gmail.com>
2026-05-15 13:28:48 -0500

    MORE

--- src/pages/Alters/Aurynn.tsx
+++ src/pages/Alters/Aurynn.tsx
@@ -2,16 +2,31 @@ import "./Aurynn.css";
 import {FivePebblesArm} from "./FivePebbles.tsx";
 
 import Pearl from "./Assets/Pearl_SU_filt.png"
-
+import Grid from "./Assets/abstract-horizontal-grid-lines-graph-style-graphic-design_1017-39918.avif"
 export default function AurynnPage() {
     return (
         <div className="Aurynn">
             <FivePebblesArm />
+            <img className="AurynnGrid" src={Grid} alt="Grid" />
+
             <div className="AurynnBackground">
                 <div className="AurynnTitle">
+                    <div className={"AurynnTitleBar"}></div>
                     <img src={Pearl} alt={"Pearl"}/>
                     <h1>Aurynn</h1>
                     <img src={Pearl} alt={"Pearl"}/>
+                    <div className={"AurynnTitleBar"}></div>
+                </div>
+
+                <div className="AurynnGrids">
+                    <div className={"AurynnGridEntry"}>
+                        <h1>Likes</h1>
+                        <p>ya</p>
+                    </div>
+                    <div className={"AurynnGridEntry"}>
+                        <h1>Likes</h1>
+                        <p>ya</p>
+                    </div>
                 </div>
 
             </div>
